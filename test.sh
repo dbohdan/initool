b9535725a267aa502b53e5b30d9e9e07847f3607
Danyil Bohdan <danyil.bohdan@gmail.com>
2015-11-06 19:37:47 +0200

    test.sh: Fix zero exit status with failed tests

--- test.sh
+++ test.sh
@@ -11,7 +11,11 @@ trap "rm -f \"$temp_file\"" 0 1 2 3 15
 for filename in $tests; do
     result_file="$(echo "$filename" | sed -e s/.command/.result/)"
     INITOOL="$initool" sh "$filename" > "$temp_file"
-    diff "$result_file" "$temp_file" || echo "in $filename" || exit_status="1"
+    if ! diff "$result_file" "$temp_file"; then
+        # The result differs from what was expected.
+        exit_status="1"
+        echo "in $filename"
+    fi
 done
 
 exit "$exit_status"
