541596f0f0206584bb93efe3dca918d6baf44dd5
Danyil Bohdan <danyil.bohdan@gmail.com>
2016-05-10 12:13:39 +0300

    build.cmd: Exit on MoSML error, MOSMLLIB, /batch
    Have the build script set the variable MOSMLLIB before running the
    compiler.
    
    Rename the flag /s "/batch".

--- build.cmd
+++ build.cmd
@@ -1,18 +1,30 @@
 @echo off
-set mosmlc1="C:\Program Files\mosml\bin\mosmlc.exe"
-set mosmlc2="C:\Program Files (x86)\mosml\bin\mosmlc.exe"
+rem Detect the path to mosmlc.exe and MOSMLLIB.
+set mosml1=C:\Program Files\mosml
+set mosml2=C:\Program Files (x86)\mosml
 
-if exist %mosmlc1% set mosmlc=%mosmlc1%
-if exist %mosmlc2% set mosmlc=%mosmlc2%
+if exist "%mosml1%\bin\mosmlc.exe" set mosml=%mosml1%
+if exist "%mosml2%\bin\mosmlc.exe" set mosml=%mosml2%
+set mosmlc="%mosml%\bin\mosmlc.exe"
+set mosmllib=%mosml%\lib\mosml
 
 if not exist %mosmlc% (
     echo Error: mosmlc.exe not found
     exit /b 1
 )
 
+rem Process the command line arguments.
+set flag_batch=0
+for %%a in (%*) do (
+    if !%%a!==!/batch! set flag_batch=1
+)
+
+rem Build initool.
 echo on
 del *.ui *.uo
 %mosmlc% -toplevel stringtrim.sml ini-sig.sml ini.sml initool.sml -o initool.exe
+@if errorlevel 1 exit /b 1
 
-@if not !%1%!==!/s! pause
+@rem Perform optional actions based on the flags that are set.
+@if !%flag_batch%!==!0! pause
 @exit /b 0
