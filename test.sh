5bd79fac1ca1f7fd08514583b93eadddb5bc93a4
D. Bohdan <dbohdan@dbohdan.com>
2023-07-25 10:17:09 +0000

    test: improve var names and err in `rec_readlink`

--- test.sh
+++ test.sh
@@ -3,11 +3,16 @@ set -eu
 
 rec_readlink() {
     rr_path=$1
-    rr_max_steps=$2
-    rr_steps=$3
+    rr_limit=$2
+    rr_depth=$3
 
-    if [ "$rr_steps" -ge "$rr_max_steps" ]; then
-        printf 'too many steps of recursion when resolving symlink\n' > /dev/stderr
+    if [ "$rr_depth" -ge "$rr_limit" ]; then
+        printf \
+            'reached recursion limit %u with depth %u when resolving symlink\n' \
+            "$rr_limit" \
+            "$rr_depth" \
+            >/dev/stderr \
+            ;
         return 1
     fi
 
@@ -18,7 +23,7 @@ rec_readlink() {
         return 0
     fi
 
-    rec_readlink "$rr_new" "$rr_max_steps" $(( rr_steps + 1 ))
+    rec_readlink "$rr_new" "$rr_limit" $((rr_depth + 1))
 }
 
 me=$(rec_readlink "$0" 10 0)
@@ -36,7 +41,7 @@ trap clean_up EXIT HUP INT QUIT TERM
 exit_status=0
 for filename in $tests; do
     result_file="$(printf '%s' "$filename" | sed -e s/.command/.result/)"
-    INITOOL="$initool" sh "$filename" > "$temp_file"
+    INITOOL="$initool" sh "$filename" >"$temp_file"
     if ! diff "$result_file" "$temp_file"; then
         # The result differs from what was expected.
         exit_status=1
